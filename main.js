84f3d0990e334632605c659c9517c7164dc75952
Walter <walteribanez555@gmail.com>
2023-04-03 13:50:54 -0400

    Template updated

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+
+toggleBtn.onclick = function (){
+    console.log("Hola");
+    dropDownMenu.classList.toggle('open');
+    const isOpen = dropDownMenu.classList.contains('open');
+
+
+    toggleBtnIcon.classList = isOpen
+    ? 'fas fa-times'
+    : 'fas fa-bars';
+
+}
